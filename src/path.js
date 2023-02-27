94734de2b8459a657d6f6263ba7e1e17dabba785
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2023-02-27 23:00:29 +0530

    update audio file paths

--- src/path.js
+++ src/path.js
@@ -1,71 +1,55 @@
-const gen = [
-    "/assets/gen/G1_1_gen_gen.wav",
-    "/assets/gen/G1_3_gen_gen.wav",
-    "/assets/gen/G1_4_gen_gen.wav",
-    "/assets/gen/G1_11_gen_gen.wav",
-    "/assets/gen/G1_12_gen_gen.wav",
-    "/assets/gen/G1_41_gen_gen.wav",
-    "/assets/gen/G1_54_gen_gen.wav",
-    "/assets/gen/G1_104_gen_gen.wav",
-    "/assets/gen/G1_107_gen_gen.wav",
-    "/assets/gen/G1_121_gen_gen.wav",
-    "/assets/gen/G1_123_gen_gen.wav",
-    "/assets/gen/G1_124_gen_gen.wav",
-    "/assets/gen/G1_129_gen_gen.wav",
-    "/assets/gen/G1_130_gen_gen.wav",
-    "/assets/gen/G1_133_gen_gen.wav",
-    "/assets/gen/G1_135_gen_gen.wav",
-    "/assets/gen/G1_142_gen_gen.wav",
-    "/assets/gen/G1_145_gen_gen.wav",
-    "/assets/gen/G1_148_gen_gen.wav",
-    "/assets/gen/G1_150_gen_gen.wav",
-    "/assets/gen/G1_1011_gen_gen.wav",
-    "/assets/gen/G1_1016_gen_gen.wav",
-    "/assets/gen/G1_1021_gen_gen.wav",
-    "/assets/gen/G1_1033_gen_gen.wav",
-    "/assets/gen/G1_1034_gen_gen.wav",
-    "/assets/gen/G1_1035_gen_gen.wav",
-    "/assets/gen/G1_1036_gen_gen.wav",
-    "/assets/gen/G1_1039_gen_gen.wav",
-    "/assets/gen/G1_1044_gen_gen.wav",
-    "/assets/gen/G1_1046_gen_gen.wav",
-    "/assets/gen/G1_1047_gen_gen.wav",
-    "/assets/gen/G1_1048_gen_gen.wav",
-]
+const kanadaToSolinga = {
+    seen: {
+        Source: [
+            "/assets/kanada to soliga/seen/1.wav",
+            "/assets/kanada to soliga/seen/3.wav",
+            "/assets/kanada to soliga/seen/101.wav"
+        ],
+        Target: [
+            "/assets/kanada to soliga/seen/G2_1_gen_gen.wav",
+            "/assets/kanada to soliga/seen/G2_3_gen_gen.wav",
+            "/assets/kanada to soliga/seen/G2_101_gen_gen.wav"
+        ]
+    },
+    unseen: {
+        Source: [
+            "/assets/kanada to soliga/unseen/2.wav",
+            "/assets/kanada to soliga/unseen/28.wav",
+            "/assets/kanada to soliga/unseen/33.wav"
+        ],
+        Target: [
+            "/assets/kanada to soliga/unseen/G2_2_gen_gen.wav",
+            "/assets/kanada to soliga/unseen/G2_28_gen_gen.wav",
+            "/assets/kanada to soliga/unseen/G2_33_gen_gen.wav"
+        ]
+    }
+}
 
-const ref = [
-    "/assets/ref/1.wav",
-    "/assets/ref/3.wav",
-    "/assets/ref/4.wav",
-    "/assets/ref/11.wav",
-    "/assets/ref/12.wav",
-    "/assets/ref/41.wav",
-    "/assets/ref/54.wav",
-    "/assets/ref/104.wav",
-    "/assets/ref/107.wav",
-    "/assets/ref/121.wav",
-    "/assets/ref/123.wav",
-    "/assets/ref/124.wav",
-    "/assets/ref/129.wav",
-    "/assets/ref/130.wav",
-    "/assets/ref/133.wav",
-    "/assets/ref/135.wav",
-    "/assets/ref/142.wav",
-    "/assets/ref/145.wav",
-    "/assets/ref/148.wav",
-    "/assets/ref/150.wav",
-    "/assets/ref/1011.wav",
-    "/assets/ref/1016.wav",
-    "/assets/ref/1021.wav",
-    "/assets/ref/1033.wav",
-    "/assets/ref/1034.wav",
-    "/assets/ref/1035.wav",
-    "/assets/ref/1036.wav",
-    "/assets/ref/1039.wav",
-    "/assets/ref/1044.wav",
-    "/assets/ref/1046.wav",
-    "/assets/ref/1047.wav",
-    "/assets/ref/1048.wav",
-]
+const soligaToKanada = {
+    seen: {
+        Source: [
+            "/assets/soliga to kanada/seen/1.wav",
+            "/assets/soliga to kanada/seen/4.wav",
+            "/assets/soliga to kanada/seen/11.wav"
+        ],
+        Target: [
+            "/assets/soliga to kanada/seen/G1_1_gen_gen.wav",
+            "/assets/soliga to kanada/seen/G1_4_gen_gen.wav",
+            "/assets/soliga to kanada/seen/G1_11_gen_gen.wav"
+        ]
+    },
+    unseen: {
+        Source: [
+            "/assets/soliga to kanada/unseen/3.wav",
+            "/assets/soliga to kanada/unseen/41.wav",
+            "/assets/soliga to kanada/unseen/54.wav"
+        ],
+        Target: [
+            "/assets/soliga to kanada/unseen/G1_3_gen_gen.wav",
+            "/assets/soliga to kanada/unseen/G1_41_gen_gen.wav",
+            "/assets/soliga to kanada/unseen/G1_54_gen_gen.wav"
+        ]
+    }
+}
 
-export { gen, ref }
+export { kanadaToSolinga, soligaToKanada }
