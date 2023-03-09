d9f110e0d47fb6da460cf04abf8e4281dbe38c16
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2023-03-09 17:39:36 +0530

    made some changes

--- src/Audio.js
+++ src/Audio.js
@@ -5,20 +5,20 @@ const Audio = ({ target }) => {
 
     return (
         <div className="mb-5">
-            <h4>{target === "kts" ? "Kanada to Soliga(Seen)" : "Soliga to Kanada(Seen)"}</h4>
+            <h4>{target === "kts" ? "Kanada to Soliga(Female to Female)" : "Soliga to Kanada(Female to Female)"}</h4>
             <div className="row">
                 <div className="col"><h5>Source</h5></div>
                 <div className="col"><h5>Target</h5></div>
             </div>
             {
-                data.seen.Source.map((audio, index) => (
+                data.Source.map((audio, index) => (
                     <div className="row" key={index}>
                         <div className="col"><audio src={audio} controls /></div>
-                        <div className="col"><audio src={data.seen.Target[index]} controls /></div>
+                        <div className="col"><audio src={data.Target[index]} controls /></div>
                     </div>
                 ))
             }
-            <br />
+            {/* <br />
             <h4>{target === "kts" ? "Kanada to Soliga(Unseen)" : "Soliga to Kanada(Unseen)"}</h4>
             <div className="row">
                 <div className="col"><h5>Source</h5></div>
@@ -31,7 +31,7 @@ const Audio = ({ target }) => {
                         <div className="col"><audio src={data.unseen.Target[index]} controls /></div>
                     </div>
                 ))
-            }
+            } */}
         </div>
     )
 }
--- src/path.js
+++ src/path.js
@@ -1,5 +1,11 @@
 const sTok = {
     Source: [
+        "/assets/g4 s2/ref/__G4_1_ref.wav",
+        "/assets/g4 s2/ref/__G4_3_ref.wav",
+        "/assets/g4 s2/ref/__G4_4_ref.wav",
+        "/assets/g4 s2/ref/__G4_11_ref.wav",
+        "/assets/g4 s2/ref/__G4_41_ref.wav",
+        "/assets/g4 s2/ref/__G4_54_ref.wav",
         "/assets/g4 s2/ref/G4_18_ref.wav",
         "/assets/g4 s2/ref/G4_19_ref.wav",
         "/assets/g4 s2/ref/G4_26_ref.wav",
@@ -7,6 +13,12 @@ const sTok = {
         "/assets/kanada_multiple_2_99_G4_S1/ref/G4_4_ref.wav",
     ],
     Target: [
+        "/assets/g4 s2/gen/__G4_1_gen.wav",
+        "/assets/g4 s2/gen/__G4_3_gen.wav",
+        "/assets/g4 s2/gen/__G4_4_gen.wav",
+        "/assets/g4 s2/gen/__G4_11_gen.wav",
+        "/assets/g4 s2/gen/__G4_41_gen.wav",
+        "/assets/g4 s2/gen/__G4_54_gen.wav",
         "/assets/g4 s2/gen/G4_18_gen.wav",
         "/assets/g4 s2/gen/G4_19_gen.wav",
         "/assets/g4 s2/gen/G4_26_gen.wav",
@@ -46,57 +58,95 @@ const kToks3 = {
 }
 
 const kanadaToSolinga = {
-    seen: {
-        Source: [
-            "/assets/kanada to soliga/seen/1.wav",
-            "/assets/kanada to soliga/seen/3.wav",
-            "/assets/kanada to soliga/seen/101.wav"
-        ],
-        Target: [
-            "/assets/kanada to soliga/seen/G2_1_gen_gen.wav",
-            "/assets/kanada to soliga/seen/G2_3_gen_gen.wav",
-            "/assets/kanada to soliga/seen/G2_101_gen_gen.wav"
-        ]
-    },
-    unseen: {
-        Source: [
-            "/assets/kanada to soliga/unseen/2.wav",
-            "/assets/kanada to soliga/unseen/28.wav",
-            "/assets/kanada to soliga/unseen/33.wav"
-        ],
-        Target: [
-            "/assets/kanada to soliga/unseen/G2_2_gen_gen.wav",
-            "/assets/kanada to soliga/unseen/G2_28_gen_gen.wav",
-            "/assets/kanada to soliga/unseen/G2_33_gen_gen.wav"
-        ]
-    }
+    Source: [
+        "/assets/kanada to soliga/seen/1.wav",
+        "/assets/kanada to soliga/seen/3.wav",
+        "/assets/kanada to soliga/seen/101.wav",
+        "/assets/kanada to soliga/unseen/2.wav",
+        "/assets/kanada to soliga/unseen/28.wav",
+        "/assets/kanada to soliga/unseen/33.wav"
+    ],
+    Target: [
+        "/assets/kanada to soliga/seen/G2_1_gen_gen.wav",
+        "/assets/kanada to soliga/seen/G2_3_gen_gen.wav",
+        "/assets/kanada to soliga/seen/G2_101_gen_gen.wav",
+        "/assets/kanada to soliga/unseen/G2_2_gen_gen.wav",
+        "/assets/kanada to soliga/unseen/G2_28_gen_gen.wav",
+        "/assets/kanada to soliga/unseen/G2_33_gen_gen.wav"
+    ]
 }
 
+// const kanadaToSolinga = {
+//     seen: {
+//         Source: [
+//             "/assets/kanada to soliga/seen/1.wav",
+//             "/assets/kanada to soliga/seen/3.wav",
+//             "/assets/kanada to soliga/seen/101.wav"
+//         ],
+//         Target: [
+//             "/assets/kanada to soliga/seen/G2_1_gen_gen.wav",
+//             "/assets/kanada to soliga/seen/G2_3_gen_gen.wav",
+//             "/assets/kanada to soliga/seen/G2_101_gen_gen.wav"
+//         ]
+//     },
+//     unseen: {
+//         Source: [
+//             "/assets/kanada to soliga/unseen/2.wav",
+//             "/assets/kanada to soliga/unseen/28.wav",
+//             "/assets/kanada to soliga/unseen/33.wav"
+//         ],
+//         Target: [
+//             "/assets/kanada to soliga/unseen/G2_2_gen_gen.wav",
+//             "/assets/kanada to soliga/unseen/G2_28_gen_gen.wav",
+//             "/assets/kanada to soliga/unseen/G2_33_gen_gen.wav"
+//         ]
+//     }
+// }
+
 const soligaToKanada = {
-    seen: {
-        Source: [
-            "/assets/soliga to kanada/seen/1.wav",
-            "/assets/soliga to kanada/seen/4.wav",
-            "/assets/soliga to kanada/seen/11.wav"
-        ],
-        Target: [
-            "/assets/soliga to kanada/seen/G1_1_gen_gen.wav",
-            "/assets/soliga to kanada/seen/G1_4_gen_gen.wav",
-            "/assets/soliga to kanada/seen/G1_11_gen_gen.wav"
-        ]
-    },
-    unseen: {
-        Source: [
-            "/assets/soliga to kanada/unseen/3.wav",
-            "/assets/soliga to kanada/unseen/41.wav",
-            "/assets/soliga to kanada/unseen/54.wav"
-        ],
-        Target: [
-            "/assets/soliga to kanada/unseen/G1_3_gen_gen.wav",
-            "/assets/soliga to kanada/unseen/G1_41_gen_gen.wav",
-            "/assets/soliga to kanada/unseen/G1_54_gen_gen.wav"
-        ]
-    }
+    Source: [
+        "/assets/soliga to kanada/seen/1.wav",
+        "/assets/soliga to kanada/seen/4.wav",
+        "/assets/soliga to kanada/seen/11.wav",
+        "/assets/soliga to kanada/seen/1.wav",
+        "/assets/soliga to kanada/seen/4.wav",
+        "/assets/soliga to kanada/seen/11.wav"
+    ],
+    Target: [
+        "/assets/soliga to kanada/seen/G1_1_gen_gen.wav",
+        "/assets/soliga to kanada/seen/G1_4_gen_gen.wav",
+        "/assets/soliga to kanada/seen/G1_11_gen_gen.wav",
+        "/assets/soliga to kanada/unseen/G1_3_gen_gen.wav",
+        "/assets/soliga to kanada/unseen/G1_41_gen_gen.wav",
+        "/assets/soliga to kanada/unseen/G1_54_gen_gen.wav"
+    ]
 }
+
+// const soligaToKanada = {
+//     seen: {
+//         Source: [
+//             "/assets/soliga to kanada/seen/1.wav",
+//             "/assets/soliga to kanada/seen/4.wav",
+//             "/assets/soliga to kanada/seen/11.wav"
+//         ],
+//         Target: [
+//             "/assets/soliga to kanada/seen/G1_1_gen_gen.wav",
+//             "/assets/soliga to kanada/seen/G1_4_gen_gen.wav",
+//             "/assets/soliga to kanada/seen/G1_11_gen_gen.wav"
+//         ]
+//     },
+//     unseen: {
+//         Source: [
+//             "/assets/soliga to kanada/seen/1.wav",
+//             "/assets/soliga to kanada/seen/4.wav",
+//             "/assets/soliga to kanada/seen/11.wav"
+//         ],
+//         Target: [
+//             "/assets/soliga to kanada/unseen/G1_3_gen_gen.wav",
+//             "/assets/soliga to kanada/unseen/G1_41_gen_gen.wav",
+//             "/assets/soliga to kanada/unseen/G1_54_gen_gen.wav"
+//         ]
+//     }
+// }
 
 export { kanadaToSolinga, soligaToKanada, sTok, kTok, kToks3 }
